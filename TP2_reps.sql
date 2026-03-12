7c76c6bee19c72f37eb1791ced36455c2cb7faaa
Lamine2004Sow <Lamine.2004pro.sow@gmail.com>
2026-03-12 23:47:14 +0100

    Réponse à question 3

--- TP2_reps.sql
+++ TP2_reps.sql
@@ -17,4 +17,12 @@ WHERE budget= (SELECT MAX(budget)
 SELECT salary, name
 FROM teacher
 WHERE salary > (SELECT AVG(salary)
-                 FROM teacher)
+                 FROM teacher)	
+                 
+-- 3. Pour chaque enseignant, afficher tous les ´etudiants qui ont suivi plus de deux cours dispens´es par cet enseignent ainsi que le nombre total de cours suivis par chaque ´etudiant, en utilisant la clause HAVING
+
+SELECT s.name, COUNT(*)
+FROM teacher tr  JOIN teaches ts ON tr.ID=ts.ID JOIN takes tk ON tk.course_id= ts.course_id JOIN student s ON s.ID= tk.ID
+
+GROUP BY(tr.name, s.name)
+HAVING(COUNT(*)) >=2
