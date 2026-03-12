d34ba454526b784ecb363e3d229b22ecb308bd48
Lamine2004Sow <Lamine.2004pro.sow@gmail.com>
2026-03-13 00:24:17 +0100

    Réponse à question 4

--- TP2_reps.sql
+++ TP2_reps.sql
@@ -21,8 +21,19 @@ WHERE salary > (SELECT AVG(salary)
                  
 -- 3. Pour chaque enseignant, afficher tous les ´etudiants qui ont suivi plus de deux cours dispens´es par cet enseignent ainsi que le nombre total de cours suivis par chaque ´etudiant, en utilisant la clause HAVING
 
-SELECT s.name, COUNT(*)
+SELECT s.name, tr.name, COUNT(*)
 FROM teacher tr  JOIN teaches ts ON tr.ID=ts.ID JOIN takes tk ON tk.course_id= ts.course_id JOIN student s ON s.ID= tk.ID
 
 GROUP BY(tr.name, s.name)
 HAVING(COUNT(*)) >=2
+
+-- 4. Pour chaque enseignant, afficher tous les ´etudiants qui ont suivi plus de deux cours dispens´es par cet enseignent ainsi que le nombre total de cours suivis par chaque ´etudiant, sans utiliser la clause HAVING.
+
+SELECT T.teachername , T.studentname , T.totalcount
+FROM ( SELECT teacher.name as teachername, student.name as studentname, count (*) as totalcount
+FROM teacher, student, takes, teaches
+WHERE teacher.id = teaches.id and student.id = takes.id and (takes.course_id, takes.sec_id, takes.semester, takes.year)=(teaches.course_id, teaches.sec_id, teaches.semester, teaches.year )
+GROUP BY teacher.name , student.name) T
+WHERE T.totalcount >= 2 ORDER BY T.teachername
+
+
