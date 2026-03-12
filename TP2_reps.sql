a40a500cd51041a9978bfc23477ab3cd5a10dcf7
Lamine2004Sow <Lamine.2004pro.sow@gmail.com>
2026-03-12 18:24:41 +0100

    Réponse à question 2

--- TP2_reps.sql
+++ TP2_reps.sql
@@ -12,4 +12,9 @@ FROM department
 WHERE budget= (SELECT MAX(budget) 
               FROM department)
               
--- 
+-- 2. Afficher les salaires et les noms des enseignants qui gagnent plus que le salaire moyen
+
+SELECT salary, name
+FROM teacher
+WHERE salary > (SELECT AVG(salary)
+                 FROM teacher)
